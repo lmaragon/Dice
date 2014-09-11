920e3806df01fd07896cfa5405b618e6ee951e34
unknown <luaragon@697-334-Rohan.sf.k12.internal>
2014-09-11 10:16:22 -0700

    Added first name to index.html

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,13 +1,29 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die one;
 Die two;
 
-void setup()
+public void setup()
 {
 	size(800,800);
 	noLoop();
 }
 
-void draw()
+public void draw()
 {
 	background (255);
 	one = new Die(100,400);
@@ -19,7 +35,7 @@ void draw()
 	text("Total Roll: "+(one.numDots+two.numDots), 400,200);
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -33,13 +49,13 @@ class Die //models one single dice cube
 		myX = x;
 		myY = y;
 	}
-	void roll()
+	public void roll()
 	{
 		numDots = (int)(Math.random()*6+1);
 	}
-	void show()
+	public void show()
 	{
-		fill(int(random(256)+50),int(random(256)+50),int(random(256)+50));
+		fill(PApplet.parseInt(random(256)+50),PApplet.parseInt(random(256)+50),PApplet.parseInt(random(256)+50));
 		rect(myX,myY,200,200, 25);
 		if (numDots == 1)
 		{
@@ -88,4 +104,13 @@ class Die //models one single dice cube
 
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
